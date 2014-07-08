79a700bd77974b3485678fd85a176e644d5e93ca
viniciuspoletto <viniciuspoletto@hotmail.com>
2014-07-07 21:47:48 -0300

    Versão 6.2

--- Servidor/tmp.php
+++ Servidor/tmp.php
@@ -1,4 +1,8 @@
 <?php
 	session_start();
-
+	$_SESSION['reserva_id'] = $_GET['id_reserva'];
+	$_SESSION['reserva_quadra_id'] = $_GET['id_quadra'];
+	$_SESSION['reserva_data'] = $_GET['data'];
+	$_SESSION['reserva_hora'] = $_GET['hora'];
+	header("Location:index.php#!/page_CadReservas");
 ?>
